a408d5a6774a7b2d846de6b9f953fdfbf0b8564b
st-wg-hzw14176 <st-wg-hzw14176@DEV3-WIN7072.BOCVIEW.COM>
2019-12-12 14:03:49 +0800

    11

--- src/main/java/com/boc/config/SwaggerConfig.java
+++ src/main/java/com/boc/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.boc.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
