b8fc3873d5eb6465acaced51efb14189802b0f29
st-wg-hzw14176 <st-wg-hzw14176@DEV3-WIN7072.BOCVIEW.COM>
2020-01-06 14:47:52 +0800

    111

--- src/main/java/com/boc/wms/demo/controller/SwaggerController.java
+++ src/main/java/com/boc/wms/demo/controller/SwaggerController.java
@@ -1,6 +1,7 @@
 package com.boc.wms.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -30,6 +31,14 @@ public class SwaggerController {
 	@GetMapping("/swa2")
 	public String test2(String id, String name) {
 		return name;
+	}
+	
+	@ApiOperation("这是一个测试接口-3") // 接口描述
+	@ApiImplicitParams({ @ApiImplicitParam(name = "id", value = "用户id", defaultValue = "1", required = true),
+			@ApiImplicitParam(name = "name", value = "用户 名字", defaultValue = "1") })
+	@PostMapping("/swa3")
+	public String test3(String id, String name) {
+		return id+name;
 	}
 
 }
